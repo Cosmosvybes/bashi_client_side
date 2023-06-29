55fea2bf5882f41f0003d3f2513580c3c887c150
Cosmos <Alfredchrisayo@gmail.com>
2023-06-29 06:51:35 -1200

    add links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,57 +1,152 @@
-import React from 'react'
-import { useState } from 'react'
-import { Link } from 'react-scroll'
-import { FaList, FaTelegram, FaTwitter, FaTimes, FaInstagram } from 'react-icons/fa'
-import logo from '../assets/mobilelogo.jpg';
-
-
-
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-scroll";
+import {
+  FaList,
+  FaTelegram,
+  FaTwitter,
+  FaTimes,
+  FaInstagram,
+} from "react-icons/fa";
+import logo from "../assets/mobilelogo.jpg";
 
 const Menu = () => {
   const [switch_, setSwitch] = useState(false);
-
+  const twitterLink = () => {
+    return (window.location.href =
+      "https://twitter.com/bashi_token/status/1671137121145442304?s=46");
+  };
+  const instaLink = () => {
+    return (window.location.href = "https://instagram.com/bashi_token");
+  };
+  const telegramLink = () => {
+    return (window.location.href = "https://t.me/+GEE7e2N50dkyZWM0");
+  };
   return (
     <>
-      {!switch_ &&
-        <div className='bars_' >
-          <button style={{ border: 'unset', backgroundColor: 'black' }}> <img src={logo} alt='logo' width={'120px'} height={'60px'} style={{ position: 'relative' }} />  </button>
-          <FaList style={{ float: 'right', fontSize: "20px", color: 'rgba(89, 205, 153, 0.682)', marginTop: '5px', marginRight: '10px' }} onClick={() => { setSwitch(!switch_) }} />
-
+      {!switch_ && (
+        <div className="bars_">
+          <button style={{ border: "unset", backgroundColor: "black" }}>
+            {" "}
+            <img
+              src={logo}
+              alt="logo"
+              width={"120px"}
+              height={"60px"}
+              style={{ position: "relative" }}
+            />{" "}
+          </button>
+          <FaList
+            style={{
+              float: "right",
+              fontSize: "20px",
+              color: "rgba(89, 205, 153, 0.682)",
+              marginTop: "5px",
+              marginRight: "10px",
+            }}
+            onClick={() => {
+              setSwitch(!switch_);
+            }}
+          />
         </div>
-      }
+      )}
 
-      <div className={switch_ ? 'overlay_' : 'overlay'}>
-
-        <nav className='content'>
-          <Link className="link" to={'roadmap'} smooth={true} duration={800}> Roadmap</Link>
-          <Link className="link" to={'referral'} smooth={true} duration={800}> Referral </Link>
-          <Link className="link" to={'why-us'} smooth={true} duration={800}> Why us </Link>
-          <Link className="link" to={'faqs'} smooth={true} duration={800}> FAQs </Link>
-          <FaTimes onClick={() => { setSwitch(!switch_) }} style={{ color: 'red', fontSize: '18px', marginTop: '3px' }} />
+      <div className={switch_ ? "overlay_" : "overlay"}>
+        <nav className="content">
+          <Link className="link" to={"roadmap"} smooth={true} duration={800}>
+            {" "}
+            Roadmap
+          </Link>
+          <Link className="link" to={"referral"} smooth={true} duration={800}>
+            {" "}
+            Referral{" "}
+          </Link>
+          <Link className="link" to={"why-us"} smooth={true} duration={800}>
+            {" "}
+            Why us{" "}
+          </Link>
+          <Link className="link" to={"faqs"} smooth={true} duration={800}>
+            {" "}
+            FAQs{" "}
+          </Link>
+          <FaTimes
+            onClick={() => {
+              setSwitch(!switch_);
+            }}
+            style={{ color: "red", fontSize: "18px", marginTop: "3px" }}
+          />
         </nav>
-      </div >
+      </div>
 
-      <div className='_container'>
-        <img src={logo} alt='logo' width={'110px'} height={'40px'} style={{ position: 'relative', marginTop: '5px', borderRadius: '3px' }} />
+      <div className="_container">
+        <img
+          src={logo}
+          alt="logo"
+          width={"110px"}
+          height={"40px"}
+          style={{
+            position: "relative",
+            marginTop: "5px",
+            borderRadius: "3px",
+          }}
+        />
 
-        <nav className='bars'>
-          <Link className='link' to={'roadmap'} smooth={true} duration={800}>  Roadmap </Link>
-          <Link className="link" to={'referral'} smooth={true} duration={800}> Referral </Link>
-          <Link className="link" to={'why-us'} smooth={true} duration={800}> Why us </Link>
-          <Link className='link' to={'faqs'} smooth={true} duration={800}> FAQs
+        <nav className="bars">
+          <Link className="link" to={"roadmap"} smooth={true} duration={800}>
+            {" "}
+            Roadmap{" "}
+          </Link>
+          <Link className="link" to={"referral"} smooth={true} duration={800}>
+            {" "}
+            Referral{" "}
+          </Link>
+          <Link className="link" to={"why-us"} smooth={true} duration={800}>
+            {" "}
+            Why us{" "}
+          </Link>
+          <Link className="link" to={"faqs"} smooth={true} duration={800}>
+            {" "}
+            FAQs
           </Link>
         </nav>
 
-
-
-        <div className='bars'>
-          <Link className='link' to={'https://t.me/+GEE7e2N50dkyZWM0'} smooth={true} duration={800}>   <FaInstagram /> </Link>
-          <Link className='link' to={'https://twitter.com/bashi_token/status/1671137121145442304?s=46'} smooth={true} duration={800}>  <FaTwitter /></Link>
-          <Link className='link' to={'https://instagram.com/bashi_token'} smooth={true} duration={800}> <FaTelegram /> </Link>
+        <div className="bars">
+          <Link
+            onClick={instaLink}
+            className="link"
+            to={"https://t.me/+GEE7e2N50dkyZWM0"}
+            smooth={true}
+            duration={800}
+          >
+            {" "}
+            <FaInstagram onClick={instaLink} />{" "}
+          </Link>
+          <Link
+            onClick={instaLink}
+            className="link"
+            to={
+              "https://twitter.com/bashi_token/status/1671137121145442304?s=46"
+            }
+            smooth={true}
+            duration={800}
+          >
+            {" "}
+            <FaTwitter onClick={twitterLink} />
+          </Link>
+          <Link
+            onClick={twitterLink}
+            className="link"
+            to={"https://instagram.com/bashi_token"}
+            smooth={true}
+            duration={800}
+          >
+            {" "}
+            <FaTelegram onClick={telegramLink} />{" "}
+          </Link>
         </div>
-      </div >
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
