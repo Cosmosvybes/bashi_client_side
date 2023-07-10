3513a1a1b5099cc510fc99cd733a8e26f70364b0
Cosmos <Alfredchrisayo@gmail.com>
2023-07-10 00:06:45 -1200

    added search engin logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,53 @@
-import './App.css'
-import Menu from './components/Menu'
-import Introduction from './components/Introduction'
-import Countdown from './components/Countdown'
-import Welcome from './components/Welcome'
-import Features from './components/Features'
-import Pie from './components/Pie'
-import Referral from './components/Referral'
-import Services from './components/Services'
-import Roadmapcarousel from './components/Roadmapcarousel'
-import Customer from './components/Customer'
-import Whyus from './components/Whyus'
-import Faq from './components/Faq'
-import Unity from './components/Unity'
-import Footer from './components/Footer'
-import Wallet from './components/Wallet'
-import { useEffect, useState } from 'react'
-import { useAccount } from 'wagmi'
-
-
-
-
+import "./App.css";
+import Menu from "./components/Menu";
+import Introduction from "./components/Introduction";
+import Countdown from "./components/Countdown";
+import Welcome from "./components/Welcome";
+import Features from "./components/Features";
+import Pie from "./components/Pie";
+import Referral from "./components/Referral";
+import Services from "./components/Services";
+import Roadmapcarousel from "./components/Roadmapcarousel";
+import Customer from "./components/Customer";
+import Whyus from "./components/Whyus";
+import Faq from "./components/Faq";
+import Unity from "./components/Unity";
+import Footer from "./components/Footer";
+import Wallet from "./components/Wallet";
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
+import { Helmet } from "react-helmet";
+import Logo from "./assets/mobilelogo.jpg";
 
 function App() {
   const { address } = useAccount();
-  const [balance, setBalance] = useState('')
+  const [balance, setBalance] = useState("");
 
-  fetch(`https://sarubashi-app.onrender.com/signup`, { method: 'POST', mode: 'cors', headers: { "Content-Type": "Application/json" }, body: JSON.stringify({ wallet: address }) })
+  fetch(`https://sarubashi-app.onrender.com/signup`, {
+    method: "POST",
+    mode: "cors",
+    headers: { "Content-Type": "Application/json" },
+    body: JSON.stringify({ wallet: address }),
+  })
     .then((response) => response.json())
     .then((res) => {
-      setBalance(res.balance)
+      setBalance(res.balance);
     });
   return (
     <>
-      <div style={{ background: 'black' }}>
-
+      <Helmet>
+        <script>
+          {`
+          {
+            "@context":"http://schema.org",
+            "@type":"Organization",
+            "url":"https://sarubashi.com",
+            "logo":${Logo}
+          }`}
+        </script>
+      </Helmet>
+      
+      <div style={{ background: "black" }}>
         <Menu />
         <Introduction userBalance={balance} />
         <Countdown />
@@ -46,14 +60,23 @@ function App() {
         <Services />
 
         <Roadmapcarousel />
-        <Customer h1={'+262k'} p="Registered users" h2="99.9%" p2="Customer satisfaction" h3="<0.10%" p3="Lowest transaction fee" h4="+34k" p4="country clients" />
+        <Customer
+          h1={"+262k"}
+          p="Registered users"
+          h2="99.9%"
+          p2="Customer satisfaction"
+          h3="<0.10%"
+          p3="Lowest transaction fee"
+          h4="+34k"
+          p4="country clients"
+        />
         <Whyus />
         <Faq />
         <Unity />
         <Footer />
-      </div >
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
