16edd82380a9cc40d70e0a0c051c1baf43fb92ef
Cosmos <Alfredchrisayo@gmail.com>
2023-07-13 02:01:35 -1200

    add link

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { useState } from "react";
 import { Link } from "react-scroll";
 import {
@@ -35,6 +35,7 @@ const Menu = () => {
               width={"120px"}
               height={"60px"}
               style={{ position: "relative" }}
+              onClick={()=>{window.location.href="sarubashi.com"}}
             />{" "}
           </button>
           <FaList
